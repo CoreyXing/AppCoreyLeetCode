77266f1ad3fbf03793cd7d504510037018e481d0
xj99051 <1634112975@qq.com>
2023-08-09 00:25:45 +0800

    append: add method

--- src/main/java/com/corey/leetcode/listnode/ListUtils.java
+++ src/main/java/com/corey/leetcode/listnode/ListUtils.java
@@ -9,59 +9,62 @@ import java.util.List;
  */
 public class ListUtils {
 
-    public static ListNode createList(int[] nums){
-        int i=0;
+    public static ListNode createList(int[] nums) {
+        int i = 0;
         ListNode pre = null;
         ListNode head = null;
-        while (i< nums.length){
+        while (i < nums.length) {
             ListNode temp = new ListNode(nums[i]);
-            if (pre != null){
+            if (pre != null) {
                 pre.next = temp;
                 head = temp;
             }
             pre = temp;
         }
-        return  head;
+        return head;
     }
 
     /**
      * 头插法
+     *
      * @param head
      * @return 是否插入成功
      */
-    public static boolean addNode2Head(ListNode head,int value){
-        if (head == null){
-            head= new ListNode(value);
+    public static boolean addNode2Head(ListNode head, int value) {
+        if (head == null) {
+            head = new ListNode(value);
             return true;
-        }else{
+        } else {
             ListNode tempNode = new ListNode(value);
-            tempNode.next=head;
-            head=tempNode;
+            tempNode.next = head;
+            head = tempNode;
             return true;
         }
     }
 
     /**
      * 链表尾插法
+     *
      * @param head
      * @param value
      * @return 是否插入成功
      */
-    public static  boolean addNode2Tail(ListNode head,int value){
-        if (head == null){
+    public static boolean addNode2Tail(ListNode head, int value) {
+        if (head == null) {
             head = new ListNode(value);
             return true;
-        }else{
-            ListNode temp =head;
-            while(temp.next != null){
-                temp=temp.next;
+        } else {
+            ListNode temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
             }
-            temp.next=new ListNode(value);
+            temp.next = new ListNode(value);
             return true;
         }
     }
 
-    public static int[] listNode2Array(ListNode head){
+
+    public static int[] listNode2Array(ListNode head) {
         // 首先计算链表的长度
         int length = 0;
         ListNode current = head;
@@ -81,5 +84,89 @@ public class ListUtils {
         return arr;
     }
 
+    public static boolean removeElement(ListNode head, int val) {
+        if (head == null) {
+            return false;
+        }
+        if (head.val == val)
+            head = head.next;
+        ListNode pre = head;
+        ListNode temp = head.next;
+        while (temp != null) {
+            if (temp.val == val) {
+                pre.next = temp.next;
+                temp = temp.next;
+            } else {
+                pre = temp;
+                temp = temp.next;
+            }
+        }
+        return true;
+    }
 
+    public static int getAtIndex(ListNode head,int index){
+        while(head ==  null){
+            return -1;//表示链表为空
+        }
+        ListNode temp =head;
+        while (index>0){
+            temp = temp.next;
+            index--;
+        }
+        return temp.val;
+    }
+
+
+    public static int getLengthOfListNode(ListNode head){
+        if (head == null){
+            return 0;
+        }
+        int len=0;
+        ListNode temp = head;
+        while (temp != null){
+            temp=temp.next;
+            len++;
+        }
+        return len;
+    }
+
+
+    public static boolean removeAtIndexOfListNode(ListNode head,int value){
+
+        if (head == null){
+            return false;
+        }
+        if (value==0){
+            head=head.next;
+            return true;
+        }
+        ListNode temp = head.next;
+        ListNode pre =head;
+        int res = 0;
+        if(value>getLengthOfListNode(head)){
+            return false;//不可能移除比链表长的元素
+        }
+        while(value>0){
+            pre=temp;
+            temp=temp.next;
+            value--;
+        }
+        pre.next= temp.next;
+        return true;
+    }
+
+
+    public static ListNode getTailValueOfListNode(ListNode head) {
+        if (head == null){
+            return null;//链表为空
+        }
+        ListNode temp = head;
+        ListNode pre=temp;
+        temp=temp.next;
+        while(temp != null){
+            pre = temp;
+            temp=temp.next;
+        }
+        return pre;
+    }
 }
